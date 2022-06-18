ffe9cb4da31aef3f9ba616336d3c665e161b5a3e
oluceps <mn1.674927211@gmail.com>
2022-06-18 13:35:18 +0800

    feat: lsp ref

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -78,10 +78,10 @@ map("n", "sc", "<C-w>c", opt)
 -- 关闭其他
 map("n", "so", "<C-w>o", opt) -- close others
 -- alt + hjkl  窗口之间跳转
-map("n", "<A-h>", "<C-w>h", opt)
-map("n", "<A-j>", "<C-w>j", opt)
-map("n", "<A-k>", "<C-w>k", opt)
-map("n", "<A-l>", "<C-w>l", opt)
+-- map("n", "<A-h>", "<C-w>h", opt)
+-- map("n", "<A-j>", "<C-w>j", opt)
+-- map("n", "<A-k>", "<C-w>k", opt)
+-- map("n", "<A-l>", "<C-w>l", opt)
 -- <leader> + hjkl 窗口之间跳转
 map("n", "<leader>h", "<C-w>h", opt)
 map("n", "<leader>j", "<C-w>j", opt)
@@ -156,8 +156,8 @@ pluginKeys.nvimTreeList = { -- 打开文件或文件夹
 }
 -- bufferline
 -- 左右Tab切换
-map("n", "<C-h>", ":BufferLineCyclePrev<CR>", opt)
-map("n", "<C-l>", ":BufferLineCycleNext<CR>", opt)
+map("n", "<S-h>", ":BufferLineCyclePrev<CR>", opt)
+map("n", "<S-l>", ":BufferLineCycleNext<CR>", opt)
 -- "moll/vim-bbye" 关闭当前 buffer
 map("n", "<leader>bc", ":Bdelete!<CR>", opt)
 map("n", "<C-w>", ":Bdelete!<CR>", opt)
