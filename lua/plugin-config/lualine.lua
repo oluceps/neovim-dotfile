589260eaf61682053eeec4fa557a6c811d1d31ec
oluceps <mn1.674927211@gmail.com>
2022-06-30 14:55:44 +0800

    ref: lualine theme

--- lua/plugin-config/lualine.lua
+++ lua/plugin-config/lualine.lua
@@ -8,7 +8,8 @@ lualine.setup({
   options = {
     -- 指定皮肤
     -- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
-    theme = "tokyonight",
+    theme = "gruvbox_dark",
+    -- theme = "everforest",
     -- 分割线
     component_separators = {
       left = "|",
